c1e975f27f1e40578bebb5b8b619271d5bff84dc
kumabiko <nousbear.dev@gmail.com>
2022-06-26 10:40:21 +0900

    React Hook useState

--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 // React Hooks Props型
 interface Props {
   text: string;
 }
+interface UserData {
+  id: number;
+  name: string;
+}
 
 const TestComponent: React.FC<Props> = (props) => {
+  // Genericsでnumberとnullを受け付ける様にする
+  const [count, setCount] = useState<number | null>(0);
+  const [user, setUser] = useState<UserData>({ id: 1, name: "dummy" });
   return (
     <div>
       <h1>{props.text}</h1>
+      <h1>{count}</h1>
     </div>
   );
 };
