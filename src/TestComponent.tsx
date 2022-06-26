2a4ee3c2ce29f1f8191f791f2fcc86513958c4ff
kumabiko <nousbear.dev@gmail.com>
2022-06-26 10:47:09 +0900

    Event handler データ型

--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -13,10 +13,18 @@ const TestComponent: React.FC<Props> = (props) => {
   // Genericsでnumberとnullを受け付ける様にする
   const [count, setCount] = useState<number | null>(0);
   const [user, setUser] = useState<UserData>({ id: 1, name: "dummy" });
+  const [inputData, setInputData] = useState("");
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setInputData(e.target.value);
+  };
+
   return (
     <div>
       <h1>{props.text}</h1>
       <h1>{count}</h1>
+      <input type="text" value={inputData} onChange={handleInputChange} />
+      <h1>{inputData}</h1>
     </div>
   );
 };
